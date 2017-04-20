94124138711558321103cbf0a23c012df64488aa
Ivan Antsiferov <voltek.dev@gmail.com>
2017-04-20 19:50:35 +0300

    Add response model.

---
+++ app/src/main/java/com/voltek/yandexmobilization/networking/entity/TranslateResponse.java
@@ -0,0 +1,12 @@
+package com.voltek.yandexmobilization.networking.entity;
+
+import java.util.List;
+
+public class TranslateResponse {
+
+    public String code;
+
+    public String lang;
+
+    public List<String> text;
+}
