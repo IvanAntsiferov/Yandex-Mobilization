8a33268cdc4885ffe76aed9522ffb0781840c23c
Ivan Antsiferov <voltek.dev@gmail.com>
2017-04-22 14:57:10 +0300

    Update history fragment layout.

--- app/src/main/java/com/voltek/yandex/mobilization/ui/history/HistoryFragment.java
+++ app/src/main/java/com/voltek/yandex/mobilization/ui/history/HistoryFragment.java
@@ -2,6 +2,7 @@ package com.voltek.yandex.mobilization.ui.history;
 
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -11,10 +12,15 @@ import com.arellomobile.mvp.presenter.PresenterType;
 import com.voltek.yandex.mobilization.R;
 import com.voltek.yandex.mobilization.ui.BaseFragment;
 
+import butterknife.BindView;
+
 public class HistoryFragment extends BaseFragment implements HistoryView {
 
     @InjectPresenter(type = PresenterType.GLOBAL, tag = "history")
     HistoryPresenter mPresenter;
+
+    @BindView(R.id.recycler_view)
+    RecyclerView mRecyclerView;
 
     // Lifecycle
     @Nullable
