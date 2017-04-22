490e9fbf72eed24f562f701a7f227ad7cb5df416
Ivan Antsiferov <voltek.dev@gmail.com>
2017-04-22 15:06:19 +0300

    Add translation adapter.

--- app/src/main/java/com/voltek/yandex/mobilization/ui/history/HistoryFragment.java
+++ app/src/main/java/com/voltek/yandex/mobilization/ui/history/HistoryFragment.java
@@ -2,6 +2,7 @@ package com.voltek.yandex.mobilization.ui.history;
 
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -11,6 +12,8 @@ import com.arellomobile.mvp.presenter.InjectPresenter;
 import com.arellomobile.mvp.presenter.PresenterType;
 import com.voltek.yandex.mobilization.R;
 import com.voltek.yandex.mobilization.ui.BaseFragment;
+
+import java.util.ArrayList;
 
 import butterknife.BindView;
 
@@ -22,16 +25,22 @@ public class HistoryFragment extends BaseFragment implements HistoryView {
     @BindView(R.id.recycler_view)
     RecyclerView mRecyclerView;
 
+    private TranslationAdapter mAdapter;
+
     // Lifecycle
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        mAdapter = new TranslationAdapter(getContext(), new ArrayList<>());
         return inflater.inflate(R.layout.fragment_history, container, false);
     }
 
     @Override
     public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
+        mRecyclerView.setHasFixedSize(true);
+        mRecyclerView.setLayoutManager(new LinearLayoutManager(getContext()));
+        mRecyclerView.setAdapter(mAdapter);
     }
 
     @Override
---
+++ app/src/main/java/com/voltek/yandex/mobilization/ui/history/TranslationAdapter.java
@@ -0,0 +1,75 @@
+package com.voltek.yandex.mobilization.ui.history;
+
+import android.content.Context;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageButton;
+import android.widget.TextView;
+
+import com.voltek.yandex.mobilization.R;
+import com.voltek.yandex.mobilization.data.entity.Translation;
+
+import java.util.List;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
+public class TranslationAdapter extends RecyclerView.Adapter<TranslationAdapter.ViewHolder> {
+
+    private List<Translation> mItems;
+
+    private Context mContext;
+
+    public TranslationAdapter(Context context, List<Translation> items) {
+        mContext = context;
+        mItems = items;
+    }
+
+    public static class ViewHolder extends RecyclerView.ViewHolder {
+
+        @BindView(R.id.ib_favorite)
+        ImageButton buttonFavorite;
+        @BindView(R.id.tv_to)
+        TextView textViewTo;
+        @BindView(R.id.tv_from)
+        TextView textViewFrom;
+        @BindView(R.id.tv_direction)
+        TextView textViewDirection;
+
+        public ViewHolder(final View itemView) {
+            super(itemView);
+            ButterKnife.bind(this, itemView);
+        }
+    }
+
+    @Override
+    public ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        Context context = parent.getContext();
+        LayoutInflater inflater = LayoutInflater.from(context);
+        View view = inflater.inflate(R.layout.item_translation, parent, false);
+        return new ViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(ViewHolder holder, int position) {
+        Translation translation = mItems.get(position);
+
+        holder.textViewTo.setText(translation.getToText());
+        holder.textViewFrom.setText(translation.getFromText());
+        holder.textViewDirection.setText(translation.getLangs());
+        if (translation.getFavorite()) {
+            holder.buttonFavorite.setImageDrawable(
+                    mContext.getResources().getDrawable(R.drawable.ic_bookmark_24dp));
+        } else {
+            holder.buttonFavorite.setImageDrawable(
+                    mContext.getResources().getDrawable(R.drawable.ic_bookmark_border_24dp));
+        }
+    }
+
+    @Override
+    public int getItemCount() {
+        return mItems.size();
+    }
+}
